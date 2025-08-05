db987063aed5fb98f0604fb940efbc71e2cc1c33
kali <kali@localhost.localdomain>
2025-08-05 14:14:01 -0400

    Move donate to the top left below other buttons

--- components/DonateButton.vue
+++ components/DonateButton.vue
@@ -53,10 +53,7 @@ const copyBtcAddress = async () => {
 <style scoped>
 .donate-btn {
     position: absolute;
-    /* top: 10px; */
-    /* left: 150px; */
     z-index: 50;
-    /* padding: 10px 20px; */
     background: linear-gradient(45deg, #ff6f61, #ffb347, #4facfe, #00f2fe);
     background-size: 400% 400%;
     color: white;
@@ -73,9 +70,9 @@ const copyBtcAddress = async () => {
     font-size: 14px;
     padding: 8px 16px;
     
-    top: 10px;
-    left: auto;
-    right: 56px;
+    top: 90px;
+    left: 10px;
+    /* right: 56px; */
 }
 
 .donate-btn:hover {
